0fd9815ba372894b871c747f34040cb4705b571e
Erlend ter Maat <erwitema@gmail.com>
2016-12-06 21:46:39 +0100

    expose my beautiful shell script to run koans with a specific ruby version

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+################################
+#
+# @author: Erlend ter Maat
+#
+################################
+
+if [ "$1" == "" ]; then
+  VER=2.3
+else
+  VER=$1
+fi
+
+echo -ne "1" > .path_progress
+source ~/.bash_profile
+rvm default $VER > /dev/null 2>&1
+rake
+echo "Using `which ruby`"
